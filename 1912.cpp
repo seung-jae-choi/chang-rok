21cf59d6b7091723351eb068efe4d6708d4bb7c2
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-03-06 20:21:25 +0900

    Create 1912.cpp

---
+++ 1912.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n;
+int dp[100001];
+int mmax(0);
+
+int main()
+{
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+		cin >> dp[i];
+	}
+	for (int i = 2; i <= n; i++) {
+		if (dp[i - 1] > 0 && dp[i - 1] + dp[i] > 0) {
+			dp[i] = dp[i - 1] + dp[i];
+		}
+		if (mmax < dp[i]) {
+			mmax = dp[i];
+		}
+	}
+	cout << mmax;
+	return 0;
+}
