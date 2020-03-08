5de56c354d44eb2f8dbe25b3dd4b1270fb282614
gksckdfhr98 <56072258+gksckdfhr98@users.noreply.github.com>
2020-03-08 20:00:04 +0900

    Update 1912.cpp

--- BOJ/1912.cpp
+++ BOJ/1912.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int n;
 int dp[100001];
-int mmax(0);
+int mmax;
 
 int main()
 {
@@ -13,6 +13,7 @@ int main()
 	for (int i = 1; i <= n; i++) {
 		cin >> dp[i];
 	}
+	mmax=dp[1];
 	for (int i = 2; i <= n; i++) {
 		if (dp[i - 1] > 0 && dp[i - 1] + dp[i] > 0) {
 			dp[i] = dp[i - 1] + dp[i];
